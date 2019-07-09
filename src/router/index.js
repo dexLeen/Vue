498271c60d141ab7e6ab36f7591cd5a369239d2e
Leen Han <Leen.Han@habatrading.com>
2019-07-09 16:51:24 +0800

    add function

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HelloWorld from "@/components/HelloWorld";
+import Index from "../views/index/Index";
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: "/",
       name: "HelloWorld",
       component: HelloWorld
+    },
+    {
+      path:"/index",
+      name: "Index",
+      component: Index
     }
   ]
 });
