b4aacacef95d895b536805efd678b178abb2744a
Leen Han <Leen.Han@habatrading.com>
2019-07-09 14:35:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <img src="./assets/images/logo.png">
+        <router-view/>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style src="./assets/styles/index.css"/>
